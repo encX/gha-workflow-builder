01f8a45dc12c569c666adab8240fe4c2639cc9d8
Plai <plai.rt.w@gmail.com>
2021-05-06 02:10:10 +0700

    Create trigger builder

---
+++ src/components/Trigger.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="trigger">
+    <h1 class="title">Triggers</h1>
+    <h2 class="subtitle">This workflow will be triggered on these events.</h2>
+
+    <div class="select">
+      <select>
+        <option value="push">Push</option>
+      </select>
+    </div>
+
+    <button class="button is-primary">Add</button>
+  </div>
+</template>
+
+<script lang="ts">
+import { Vue, Component } from "vue-property-decorator";
+
+@Component
+export default class Trigger extends Vue {}
+</script>
+
+<style lang="scss" scoped></style>
